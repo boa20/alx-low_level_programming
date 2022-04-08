a1d445c141dc076df8aca389cc8b17682eae2873
boa20 <kwesikyeremateng5@gmail.com>
2022-04-07 18:25:22 -0700

    Intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program execution starts here
+ * quote test
+ * *
+ * Return: 1 should be returned after program execution
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
