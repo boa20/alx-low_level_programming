8262e0fdf850745d2aa843270f28eb4723df3195
boa20 <kwesikyeremateng5@gmail.com>
2022-04-07 17:34:02 -0700

    Print to screen using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-/*This is the main function*/
+/**
+ *  *main - entry point
+ *   *
+ *    *Return: 0 after printing the function
+ *     
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
