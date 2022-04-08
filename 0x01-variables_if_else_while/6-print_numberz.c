ff00e4292cf2e0a71b2d2803d3caca9c3764e317
boa20 <kwesikyeremateng5@gmail.com>
2022-04-08 16:18:18 -0700

    Display single digits using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 10)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
