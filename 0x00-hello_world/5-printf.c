ed0244144033dd9a7b65cb08f40474fe673f67e1
boa20 <kwesikyeremateng5@gmail.com>
2022-04-07 17:49:04 -0700

    Print to screen using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * Execution begins here
+ * main - Execution begins here
+ * printf task
  * *
  * Return: 0 must be returned
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
