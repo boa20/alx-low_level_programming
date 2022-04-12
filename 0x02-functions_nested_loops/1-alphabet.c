6824f652ec37a2459405145ff9b5040fa24d05ca
boa20 <kwesikyeremateng5@gmail.com>
2022-04-12 16:06:08 -0700

    Display alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,22 @@
-#include
+#include "main.h"
+/**
+ * main - checks function created
+ * *
+ * Return: Always return 0
+ */
+
+void print_alphabet(void)
+{
+	char a = 'a';
+	while (a <= 'z')
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
