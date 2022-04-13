2317ec257aae549c752ece48754b7667929eab12
boa20 <kwesikyeremateng5@gmail.com>
2022-04-13 06:08:45 -0700

    Display 9-times table starting from 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,14 +15,19 @@ void times_table(void)
 		for (b = 0; b < 10; b++)
 		{
 			product = a * b;
-			if (product < 10)
+			if (b == 0)
 			{
+				_putchar('0');
+			}
+
+			else if (product < 10)
+			{
+				_putchar(' ');
 				_putchar(' ');
 				_putchar((product % 10) + '0');
 				if (b < 9)
 				{
 					_putchar(',');
-					_putchar(' ');
 				}
 			}
 
