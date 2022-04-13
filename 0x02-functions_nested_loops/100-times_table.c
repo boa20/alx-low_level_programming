fe6cc6ee5ba9eb038c4999c7a587cf7778a3a804
boa20 <kwesikyeremateng5@gmail.com>
2022-04-13 15:51:45 -0700

    Updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,20 +8,18 @@ void print_times_table(int n)
 {
 	int a;
 	int b;
-	int product;
-
+	
 	if ((n <= 15) && (n >= 0))
 	{
 		for (a = 0; a <= n; a++)
 		{
 			for (b = 0; b <= n; b++)
 			{
-				product = a * b;
+				int product = a * b;
 				if (b == 0)
 				{
 					_putchar('0');
 				}
-
 				else if (product <= 9)
 				{
 					_putchar(',');
@@ -30,7 +28,6 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar((product % 10) + '0');
 				}
-
 				else if (product >= 100)
 				{
 					_putchar(',');
@@ -39,7 +36,6 @@ void print_times_table(int n)
 					_putchar((product / 10) + '0');
 					_putchar((product % 10) + '0');
 				}
-
 				else
 				{
 					_putchar(',');
