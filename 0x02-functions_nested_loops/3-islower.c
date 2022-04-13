4cef54e4d5f6aed5aaaa468cb34965eacfab7383
boa20 <kwesikyeremateng5@gmail.com>
2022-04-12 17:08:24 -0700

    Display 1 if charaacter is lower and 0 otherwise

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - checks for lowercase character
- * *
+ * @c: contains value to be compared
  * Return: Returns 1 if lower and 0 otherwise
  */
 int _islower(int c)
