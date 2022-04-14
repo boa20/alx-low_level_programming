3139b3781a76449d7cd9fac77aa1aa4dcba887de
boa20 <kwesikyeremateng5@gmail.com>
2022-04-14 15:34:52 -0700

    Return 1 when alphabet is uppercase and 0 otherwise

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - Checks for uppercase alphabets
+ *
+ * Return: Returns 1 when upper and 0 otherwise.
+ */
+int _isupper(int c)
+{
+	char a;
+
+	if (a >= 'A' && a <= 'Z')
+	{
+		 return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
