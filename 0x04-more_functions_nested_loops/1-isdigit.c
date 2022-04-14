8d0b089e60e0a9dba33f3c600561b7b99aea1808
boa20 <kwesikyeremateng5@gmail.com>
2022-04-14 16:18:01 -0700

    Return 1 when alphabet is uppercase and 0 otherwise

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - Checks for uppercase alphabets
+ *@c: character to be checked
+ * Return: Returns 1 when upper and 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
