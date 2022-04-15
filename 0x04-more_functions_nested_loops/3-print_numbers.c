5fc1558215ce69db326bb8650d4ff57fe1444b8e
boa20 <kwesikyeremateng5@gmail.com>
2022-04-15 14:53:10 -0700

    Displays numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print numbersfrom 0 to 9
+ * 
+ * Return: Returns 0
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		_putchar(x + '0');
+	}
+
+	_putchar('\n');
+	return (0);
+}
