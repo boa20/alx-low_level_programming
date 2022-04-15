85c037be6ff365aebae57e57835273e2bd568a02
boa20 <kwesikyeremateng5@gmail.com>
2022-04-15 16:41:53 -0700

    Displays a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_square - print a square
+ *@n: number of times '#' is to be printed
+ **
+ */
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			y = 0;
+			while (y < size)
+			{
+				_putchar('#');
+				y++;
+			}
+			_putchar('\n');
+		}
+	}
+}
