60d0169a36aab9fe04c6ce443749f81dbfae8c25
boa20 <kwesikyeremateng5@gmail.com>
2022-04-20 22:39:35 -0700

    Update the value of its parameter to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * reset_to_98 - updates the value of its parameter to 98
- * @*n: represents the pointer used to reset a value
+ * @n: represents the pointer used to reset a value
  * Return: nothing
  */
-void reset_to_98 (int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
