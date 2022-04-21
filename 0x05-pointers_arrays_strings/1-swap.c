f31b04ef4b6d6bf3afd19a7efab8516c6a1ea642
boa20 <kwesikyeremateng5@gmail.com>
2022-04-20 22:46:00 -0700

    Swap values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value of its parameter to 98
+ * @a: represents the one of the pointers used to swap the values of two integers
+ * @b: represents the one of the pointers used to swap the values of two integers
+ * Return: nothing
+ */
+void reset_to_98(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
