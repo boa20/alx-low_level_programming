b2d8bd52fae59887b56a064443e15fbeefe0a86e
boa20 <kwesikyeremateng5@gmail.com>
2022-04-21 17:16:18 -0700

    Displaya string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer to a string
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int l;
+
+	for (l = 0; s[l] != '\0'; l++)
+	{
+	}
+	while (l > 0)
+	{
+		_putchar(s[l]);
+		l++;
+	}
+	_putchar('\n');
+}
