d557dce7df783230e67f3c46dff48fe21705faf9
boa20 <kwesikyeremateng5@gmail.com>
2022-04-27 16:23:24 -0700

    Add numbers in a diagonal

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+ #include "main.h"
+/**
+ * print_diagsums- adds numbers in a diagonal
+ * @a: array containing characters
+ * @size: size of array levels
+ * Return: nothing
+ */
+void print_diagsums(int *a, int size)
+{
+	int b;
+	int c = 0;
+	int d = 0;
+	int e = size * size;
+
+	for (b = 0; b < e; b++)
+	{
+		c += a[b];
+	}
+	for (b = size - 1; b < s - 1; b += size -1)
+	{
+		d += a[b];
+	}
+	printf("%d, %d\n", c, d);
+}
