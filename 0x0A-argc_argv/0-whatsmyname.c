16e9dc4e2da11f9c695b508f3e9a50c8599c94fa
boa20 <kwesikyeremateng5@gmail.com>
2022-05-04 22:53:44 -0700

    Displays its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  * @argv: the argument vector
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
