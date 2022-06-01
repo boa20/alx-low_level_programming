1d3928ab75d8bf7429ffc61678268f76b9e659ad
boa20 <kwesikyeremateng5@gmail.com>
2022-06-01 12:44:42 -0700

    Binary to unit file updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,7 +17,9 @@ unsigned int binary_to_uint(const char *b)
 	while (b[i])
 		i++;
 	i--;
-	while (i >= 0)									{											if (b[i] != '0' && b[i] != '1')
+	while (i >= 0)
+	{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		if (b[i] == '1')
 			sum += power;
